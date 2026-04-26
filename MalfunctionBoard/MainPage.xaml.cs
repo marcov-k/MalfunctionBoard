508ac458b1ae88d09a278d50c7f8b505d4325509
Marco Vasko-Klima <marcoaklima@gmail.com>
2026-04-25 21:13:31 -0400

    Added variable display element dimensions

--- MalfunctionBoard/MainPage.xaml.cs
+++ MalfunctionBoard/MainPage.xaml.cs
@@ -4,72 +4,146 @@
 
     public partial class MainPage : ContentPage
     {
-        Grid MainGrid;
+        readonly Grid MainGrid;
+        readonly Dictionary<GridPos, DashboardDisplay> GridDisplays = [];
+        readonly Dictionary<string, DashboardDisplay> DisplayBindings = [];
+        const int RowCount = 4;
+        const int ColumnCount = 6;
+        const double RowSpacing = 10;
+        const double ColumnSpacing = 10;
+        readonly Thickness Margin = new(10);
+        readonly Color CellColor = Colors.DarkGray;
+        const double TitleSize = 20;
+        const double ValueSize = 30;
 
         public MainPage()
         {
-            int rowCount = 4;
-            int columnCount = 6;
-            double rowSpacing = 10;
-            double columnSpacing = 10;
-            var margin = new Thickness(10);
-            var cellColor = Colors.DarkGray;
-
-            double titleSize = 20;
-            double valueSize = 30;
-
-            var rowDef = new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) };
-            var colDef = new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) };
-
             MainGrid = new Grid()
             {
                 RowDefinitions = {},
-                RowSpacing = rowSpacing,
+                RowSpacing = RowSpacing,
                 ColumnDefinitions = {},
-                ColumnSpacing = columnSpacing,
-                Margin = margin
+                ColumnSpacing = ColumnSpacing,
+                Margin = Margin
             };
 
-            for (int i = 0; i < rowCount; i++)
+            for (int i = 0; i < RowCount; i++)
             {
-                MainGrid.RowDefinitions.Add(rowDef);
+                MainGrid.RowDefinitions.Add(new()
+                {
+                    Height = new GridLength(1, GridUnitType.Star)
+                });
             }
-            for (int i = 0; i < columnCount; i++)
+            for (int i = 0; i < ColumnCount; i++)
             {
-                MainGrid.ColumnDefinitions.Add(colDef);
+                MainGrid.ColumnDefinitions.Add(new()
+                {
+                    Width = new GridLength(1, GridUnitType.Star)
+                });
             }
 
-            for (int row = 0; row < MainGrid.RowDefinitions.Count; row++)
+            Content = MainGrid;
+
+            if (AddDisplay<DoubleDisplay>("test1", out var display1))
             {
-                for (int col = 0; col < MainGrid.ColumnDefinitions.Count; col++)
+                display1.Title = "Double Display 1";
+                display1.TitleSize = TitleSize;
+                display1.Value = 20.0;
+                display1.ValueSize = ValueSize;
+                display1.BackgroundColor = CellColor;
+            }
+
+            if (AddDisplay<StringDisplay>("test2", out var display2, new(6, 3)))
+            {
+                display2.Title = "String Display 1";
+                display2.TitleSize = TitleSize;
+                display2.Value = "Test String";
+                display2.ValueSize = ValueSize;
+                display2.BackgroundColor = CellColor;
+            }
+        }
+
+        public bool AddDisplay<T>(string binding, out T display, GridDims? dimensions = null) where T : DashboardDisplay, IHasGridDims, new()
+        {
+            display = new();
+            dimensions ??= display.Dimensions;
+            if (!DisplayBindings.ContainsKey(binding) && FindOpenPosition(dimensions.Width, dimensions.Height, out var positions))
+            {
+                foreach (var pos in positions)
                 {
-                    MainGrid.Add(new BoxView()
+                    GridDisplays.Add(pos, display);
+                }
+                MainGrid.SetRow(display, positions[0].Row);
+                MainGrid.SetRowSpan(display, dimensions.Height);
+                MainGrid.SetColumn(display, positions[0].Col);
+                MainGrid.SetColumnSpan(display, dimensions.Width);
+
+                DisplayBindings.Add(binding, display);
+
+                MainGrid.Add(display);
+                return true;
+            }
+            return false;
+        }
+
+        bool FindOpenPosition(int width, int height, out List<GridPos> positions)
+        {
+            if (GridDisplays.Count != RowCount * ColumnCount)
+            {
+                for (int row = 0; row < RowCount - (height - 1); row++)
+                {
+                    for (int col = 0; col < ColumnCount - (width - 1); col++)
                     {
-                        Color = cellColor
-                    }, row, col);
+                        GridPos start = new(row, col);
+
+                        if (ValidPosition(width, height, start, out positions)) return true;
+                    }
                 }
             }
 
-            MainGrid.Add(new DoubleDisplay()
-            {
-                Title = "Double Display 1",
-                TitleSize = titleSize,
-                Value = 20.0,
-                ValueSize = valueSize
-            }, 0, 0);
-            MainGrid.Add(new DoubleDisplay()
-            {
-                Title = "Double Display 2",
-                TitleSize = titleSize,
-                Value = 10.0,
-                ValueSize = valueSize
-            }, 1, 2);
-
-            Content = MainGrid;
+            positions = [];
+            return false;
         }
 
-        abstract partial class DashboardDisplay : ContentView
+        bool ValidPosition(int width, int height, GridPos start, out List<GridPos> positions)
         {
+            positions = [];
+            for (int row = 0; row < height; row++)
+            {
+                for (int col = 0; col < width; col++)
+                {
+                    positions.Add(new(start.Row + row, start.Col + col));
+                }
+            }
+
+            foreach (var pos in positions)
+            {
+                if (GridDisplays.ContainsKey(pos)) return false;
+            }
+
+            return true;
+        }
+
+        record GridPos(int Row, int Col)
+        {
+            public int Row { get; } = Math.Clamp(Row, 0, RowCount - 1);
+            public int Col { get; } = Math.Clamp(Col, 0, ColumnCount - 1);
+        }
+
+        public record GridDims(int Width, int Height)
+        {
+            public int Width { get; } = Math.Clamp(Width, 1, ColumnCount);
+            public int Height { get; } = Math.Clamp(Height, 1, RowCount);
+        }
+
+        public interface IHasGridDims
+        {
+            GridDims Dimensions { get; }
+        }
+
+        public partial class DashboardDisplay : ContentView, IHasGridDims
+        {
+            public virtual GridDims Dimensions => new(1, 1);
             public string Title
             {
                 get => (string)GetValue(TitleProperty);
@@ -114,7 +188,7 @@
             }
         }
 
-        abstract partial class ValueDisplay<T> : DashboardDisplay
+        public partial class ValueDisplay<T> : DashboardDisplay
         {
             public T Value
             {
@@ -146,10 +220,9 @@
             }
         }
 
-        partial class IntDisplay : ValueDisplay<int> { }
+        public partial class IntDisplay : ValueDisplay<int> { }
 
-        partial class DoubleDisplay : ValueDisplay<double> { }
-
-        partial class StringDisplay : ValueDisplay<string> { }
+        public partial class DoubleDisplay : ValueDisplay<double> { public override GridDims Dimensions => new(2, 1); }
+        public partial class StringDisplay : ValueDisplay<string> { }
     }
 }
